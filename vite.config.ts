fd6524aa38283df0fff9106913c87cebb6217b3b
vamsidulam <151850002+vamsidulam@users.noreply.github.com>
2025-09-04 01:04:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -26,4 +26,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    include: ['@clerk/clerk-react'] // <-- add this line
+  }
 }));
